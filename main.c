d0069336c805bb537473288913bcfaf0be50731a
vortex2jm <joaopaulomoura5328@gmail.com>
2022-08-06 17:50:29 -0300

    :fire: Removing comments from main

--- main.c
+++ main.c
@@ -31,20 +31,5 @@ int main(int argc, char *argv[]) {
   encodedText = EncodeText(table, text);
   printf("texto codificado => %s\n", encodedText);
 
-  // List *list = CreateVoidList();
-
-  // Tree *tree1, *tree2, *tree3, *tree4, *tree5;
-
-  // tree1 = CreateInternalNode(CreateVoidTree(), CreateVoidTree(), 1);
-  // tree2 = CreateInternalNode(CreateVoidTree(), CreateVoidTree(), 1);
-  // tree3 = CreateInternalNode(CreateVoidTree(), CreateVoidTree(), 2);
-  // tree4 = CreateInternalNode(CreateVoidTree(), CreateVoidTree(), 3);
-  // tree5 = CreateInternalNode(CreateVoidTree(), CreateVoidTree(), 0);
-
-  // ListPush(list, tree1);
-  // ListPush(list, tree2);
-  // ListPush(list, tree3);
-  // ListPush(list, tree4);
-  // ListPush(list, tree5);
   return 0;
 }
