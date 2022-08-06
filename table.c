8198cc850156df8b335c216f8a2fd18c583c883a
vortex2jm <joaopaulomoura5328@gmail.com>
2022-08-06 11:34:53 -0300

    :recycle: adjusting some lines

--- table.c
+++ table.c
@@ -4,18 +4,27 @@
 
 int *CreateFrequencyTable(char *fileWay) {
 
-  static int table[256] = {0};
+  static int table[SIZE] = {0};
   char read_char = '\0';
   FILE *file = fopen(fileWay, "r");
+  int index = 0;
 
   while (1) {
     if (feof(file)) {
       table[read_char]--;
       break;
     }
-
     fscanf(file, "%c", &read_char);
-    table[read_char]++;
+    table[read_char]+=1;
   }
   return table;
+}
+
+void PrintFrequencyTable(int * table){
+
+  printf("\n====Tabela de frequencia====\n\n");
+
+  for(int x=0;x<SIZE;x++){
+    printf("%d -> %d\n", x, table[x]);
+  }
 }
--- table.h
+++ table.h
@@ -1,6 +1,9 @@
 #ifndef file_manager_h
 #define file_manager_h
+#define SIZE 256
 
 int *CreateFrequencyTable(char *fileWay);
+
+void PrintFrequencyTable(int * table);
 
 #endif // !file_manager_h
